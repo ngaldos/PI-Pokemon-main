5f569ecbb15c39da9c8c0892b5061e20e2a8f09c
ngaldos <ngaldos98@gmail.com>
2023-04-21 02:53:03 -0300

    Un poco mas

--- client/src/views/about.jsx
+++ client/src/views/about.jsx
@@ -7,7 +7,7 @@ const About = ()=>{
             <Nav/>
             <div className={style.text}>
                 <h3>Website made by Nicolas Galdos</h3>
-                <p>This website was made putting into practice everything I've learn from Soy Henry.</p>
+                <p>This website was made by putting into practice everything I've learned from Soy Henry.</p>
                 <h5>That includes tecnologies like React, Redux, Axios, HTML, CSS, ES6, Express and Insomnia. Preparing myself to become a FullStack developer.</h5>
             </div>
         </div>
--- client/src/views/detail.jsx
+++ client/src/views/detail.jsx
@@ -1,22 +1,26 @@
+import style from './detail.module.css';
+import Nav from '../components/nav/nav';
+
 const Detail = (props)=>{
     const {id, name, img, health, attack, defense, speed, height, weight, type}= props;
 
     return(
         <div>
-            <img src={img}/>
-            <div>
-                <h2>ID: {id}</h2>
-                <h2>Name: {name}</h2>
-                <h3>Health: {health}</h3>
-                <h3>Attack: {attack}</h3>
-                <h3>Defense: {defense}</h3>
-                <h3>Speed: {speed}</h3>
-                <h3>Height: {height}</h3>
-                <h3>weight: {weight}</h3>
-                <h3>Types: {type}</h3>
+            <Nav/>
+            <div className={style.higher}>
+                <img src={img}/>
+                <div className={style.text}>
+                    <h2>ID: {id}</h2>
+                    <h2>Name: {name}</h2>
+                    <h3>Health: {health}</h3>
+                    <h3>Attack: {attack}</h3>
+                    <h3>Defense: {defense}</h3>
+                    <h3>Speed: {speed}</h3>
+                    <h3>Height: {height}</h3>
+                    <h3>weight: {weight}</h3>
+                    <h3>Types: {type}</h3>
+                </div>
             </div>
-
-
         </div>
     );
 }
