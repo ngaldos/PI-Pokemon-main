0c6760fc34ca190d9f0c1aca380f877403c8cb25
unknown <ngaldos98@gmail.com>
2023-09-09 05:23:20 -0300

    Little bit of content

--- client/src/views/myProfile/myProfile.jsx
+++ client/src/views/myProfile/myProfile.jsx
@@ -1,8 +1,22 @@
+import { useSelector } from "react-redux";
+import {Link} from 'react-router-dom';
+import Nav from '../../components/nav/nav';
+
 const MyProfile = ()=>{
+    const user = useSelector((state)=> state.user);
 
     return (
         <>
-            <h1>MY PROFILE</h1>
+            <Nav/>
+            <div className="div--info">
+                {!!user?.mail ? <>
+                    <h2>Name: {user?.name}</h2>
+                    <h2>Last name: {user?.lastName}</h2>
+                    <h2>E-Mail: {user?.mail}</h2>
+                </> : <>
+                    <h1>Please, sign in with a user or sign up </h1>
+                </>}
+            </div>
         </>
     );
 }
