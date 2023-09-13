5b3d9d9f1cbd13cb5418c5bd02c81fbddf08de6c
unknown <ngaldos98@gmail.com>
2023-09-13 13:24:42 -0300

    hotfix

--- api/src/handlers/favsHandler.js
+++ api/src/handlers/favsHandler.js
@@ -36,7 +36,10 @@ const getUserFavsHandler = async (req, res)=>{
         const response = await getUserFavs(id);
         res.status(200).json(response);
     } catch (error) {
-        res.status(500).send(error.message);
+        if (error.message == `We found no user with that Id.`)
+            res.status(404).send(error.message)
+        else
+            res.status(500).send(error.message);
     }
 }
 
